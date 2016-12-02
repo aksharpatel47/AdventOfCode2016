c82e9362ed63a30c456fd37631e5e94a7d08da27
Akshar Patel <akshar.patel.47@gmail.com>
2016-12-02 10:16:25 +0530

    Initial Commit

---
+++ AdventOfCode2016/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdventOfCode2016
+//
+//  Created by Techniexe on 02/12/16.
+//  Copyright © 2016 Ekantik Tech. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
